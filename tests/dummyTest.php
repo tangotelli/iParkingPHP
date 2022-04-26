58bf92592f559a44be13602ab2ed659a622ca06f
Alvaro Tango Fernandez <alvaro.tangofdez@gmail.com>
2022-04-26 11:33:18 +0200

    test that works so actions does not fail

--- tests/dummyTest.php
+++ tests/dummyTest.php
@@ -9,6 +9,6 @@ class dummyTest extends TestCase
     public function testConstructor(): void
     {
         $num = 1;
-        self::assertSame($num, 2);
+        self::assertSame($num, 1);
     }
 }
